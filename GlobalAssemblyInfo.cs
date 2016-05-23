46cd3fc5813e2746d49f2eda80bf2ad52cbda713
andybisson <microsoft@abisson.co.uk>
2016-05-23 11:49:27 +0100

    More license changes

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.1.1.1")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0")]
